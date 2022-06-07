d8ead4f19ca917d12909e03efa9d9cbe931431a2
claretnnamocha <devclareo@gmail.com>
2022-06-07 05:56:37 +0100

    validate that "coins" distribution evaluates to exactly 100%

--- dca.py
+++ dca.py
@@ -2,8 +2,30 @@ import json
 from binance.client import Client
 
 config = json.load(open("config.json"))
-amount, fee = config["amount"], config['fee']
+amount, fee = config["amount"], config["fee"]
 client = Client(config["keys"]["api"], config["keys"]["secret"])
+coins = config["coins"]
+
+
+def init_dca():
+    validate_distributions()
+    print(f"Purchase Amount: ${amount:.2f}\nFee Rate: ${fee / 100 * amount:.2f}\n")
+
+    for coin in coins:
+        purchasing_amount = coins[coin] / 100 * amount
+        total_purchase_amount = purchasing_amount + (fee / 100 * purchasing_amount)
+        print(f"Attempting to buy ${total_purchase_amount:.2f} of ${coin.upper()}")
+        place_order(coin, total_purchase_amount)
+
+
+def validate_distributions():
+    percentage = 0
+
+    for coin in coins:
+        percentage += coins[coin]
+
+    if percentage != 100:
+        raise ValueError("Coins allocation must add up to exactly 100")
 
 
 def place_order(ticker, amount):
@@ -16,21 +38,16 @@ def place_order(ticker, amount):
         symbol=f"{ticker}USDT".upper(),
         side=Client.SIDE_BUY,
         type=Client.ORDER_TYPE_MARKET,
-        quoteOrderQty=round(amount, 3)
+        quoteOrderQty=round(amount, 3),
     )
 
     if config["testing"]:
         print("Real orders cannot be made in testing mode")
     else:
         print(
-            f"Amount Bought: {res['executedQty']}\nPrice: ${1 / float(res['executedQty']) * float(res['cummulativeQuoteQty'])}\nTotal Spent: ${res['cummulativeQuoteQty']}\n\n")
+            f"Amount Bought: {res['executedQty']}\nPrice: ${1 / float(res['executedQty']) * float(res['cummulativeQuoteQty'])}\nTotal Spent: ${res['cummulativeQuoteQty']}\n\n"
+        )
 
 
-print(f"Purchase Amount: ${amount:.2f}\nFee Rate: ${fee / 100 * amount:.2f}\n")
-
-coins = config["coins"]
-for coin in coins:
-    purchasing_amount = coins[coin] / 100 * amount
-    total_purchase_amount = purchasing_amount + (fee / 100 * purchasing_amount)
-    print(f"Attempting to buy ${total_purchase_amount:.2f} of ${coin.upper()}")
-    place_order(coin, total_purchase_amount)
+if __name__ == "__main__":
+    init_dca()
